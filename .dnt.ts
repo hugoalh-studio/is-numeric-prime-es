799990dd60cf18349630b27ec8528b7d5b227e01
hugoalh <hugoalh@gmail.com>
2024-10-19 16:58:55 +0800

    Update resources

--- .dnt.ts
+++ .dnt.ts
@@ -8,11 +8,11 @@ await invokeDenoNodeJSTransformer({
 		"LICENSE.md",
 		"README.md"
 	],
-	entrypoints: configJSR.exports,
+	entrypoints: configJSR.getExports(),
 	generateDeclarationMap: true,
 	metadata: {
-		name: "@hugoalh/is-numeric-prime",
-		version: configJSR.version,
+		name: configJSR.getName(),
+		version: configJSR.getVersion(),
 		description: "A module to determine whether the numeric is prime.",
 		keywords: [
 			"is",
